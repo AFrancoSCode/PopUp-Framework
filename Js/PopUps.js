41c2d3bbd9121ecb25743e278bf527be565778c4
unknown <pipe56482.p@gmail.com>
2021-12-13 11:59:33 -0500

    Framework mejorado pero estilizado

--- Js/PopUps.js
+++ Js/PopUps.js
@@ -1,75 +1,76 @@
-let list = document.querySelectorAll('#popButton');
+let list = document.querySelectorAll('#p-Button');
 
 list.forEach(function(e){
     e.addEventListener('click', function(){
-        let desingPop = new DesignPop(e);
-        let content = desingPop.setContainer(e);
-        
-        content.classList.add('popUp-Content-r');
+        let pop = new Pop();
+        let content = pop.setTotalContainer(e);
+        let contentPop = pop.setPopContainer(e);
 
-        desingPop.popSetAttributes(content.classList);
-        content.addEventListener('click', function(e){
-            desingPop.popSetEvents(e.target, content);
+        pop.popSetAttributes(contentPop.classList);
+
+        contentPop.addEventListener('click', function(e){
+            pop.popSetEvents(e.target, contentPop);
         });
-        if(content.querySelector('#overload')){
-            content.querySelector('#overload').value = "";
+        if(content.querySelector('#p-overload')){
+            content.querySelector('#p-overload').value = "";
         }
     });
 });
 
-class DesignPop{
-    constructor(e) {
-        this.e = e;
-    }
-    
+class Pop{
     setTotalContainer(e){
         let buttonContainer = e;
-        while(!buttonContainer.classList.contains('popUp-Container')){
+        while(!buttonContainer.classList.contains('p-Container')){
             buttonContainer = buttonContainer.parentElement;
         }
         return buttonContainer;
     }
-
-    setContainer(e){
+    setPopContainer(e){
         let buttonContainer = this.setTotalContainer(e);
-        return buttonContainer.querySelector('.popUp-Content');
+        // if(buttonContainer.querySelector('.p-Slim')){
+        //     return buttonContainer.querySelector('.p-Slim');
+        // }
+        return buttonContainer.querySelector('.p-Content');
     }
-
     popSetAttributes(listclass){
-        if(listclass.contains('popUp-center')){
-            listclass.add('popUp-Content-center');
+        if(!listclass.contains('.p-Content-v')){
+            listclass.add('p-Content-v');
+            // if(listclass.contains('p-Slim')){
+            //     listclass.add('p-Slim-v');
+            // }
+            // else if(listclass.contains('p-Content')){
+            //     listclass.add('p-Content-v');
+            // }
         }
-        if(listclass.contains('temp-popUp')){
-            setTimeout(function(){listclass.remove('popUp-Content-r')}, 3000);
-        }
-        if(listclass.contains('notBlur')){
-            listclass.add('popUp-Content-notblur');
+        if(listclass.contains('p-Temp')){
+            setTimeout(function(){
+                listclass.remove('p-Content-v');
+            }, 3500);
         }
     }
-
     popSetEvents(e, container){
-        if((e.classList.contains('popUp-Content') || e.id == 'close') && !e.classList.contains('Unclose')){
-            container.classList.remove('popUp-Content-r');
+        if(e.id == 'p-Close'){
+            container.classList.remove('p-Content-v');
         }
-        if(e.id == 'submit'){
-            let download = container.parentElement.querySelector('#download');
-            download.value = container.querySelector('#overload').value;
-            container.classList.remove('popUp-Content-r');
+        if(e.id == 'p-Submit'){
+            let download = container.parentElement.querySelector('#p-download');
+            download.value = container.querySelector('#p-overload').value;
+            container.classList.remove('p-Content-v');
         }
-        if(e.id == 'autoSubmit'){
-            let download = container.parentElement.querySelector('#download');
+        if(e.id == 'p-autoSubmit'){
+            let download = container.parentElement.querySelector('#p-download');
             download.value = e.value;
-            container.classList.remove('popUp-Content-r');
+            container.classList.remove('p-Content-v');
         }
-        if(e.id == 'outautoSubmit'){
-            let download = container.parentElement.querySelector('#download');
-            download.innerHTML = e.value;
-            container.classList.remove('popUp-Content-r');
-        }
-        if(e.id == 'outSubmit'){
-            let download = container.parentElement.querySelector('#download');
-            download.innerHTML = container.querySelector('#overload').value;
-            container.classList.remove('popUp-Content-r');
+        // if(e.id == 'p-outautoSubmit'){
+        //     let download = container.parentElement.querySelector('#p-download');
+        //     download.innerHTML = e.value;
+        //     container.classList.remove('p-Content-v');
+        // }
+        if(e.id == 'p-outSubmit'){
+            let download = container.parentElement.querySelector('#p-download');
+            download.innerHTML = container.querySelector('#p-overload').value;
+            container.classList.remove('p-Content-v');
         }
     }
 }
